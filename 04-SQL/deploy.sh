624d8aec77b143f0f0a2fe22fe64d722730e0731
jbosso <j_bosso@outlook.com>
2021-12-19 12:53:30 +0000

    message

--- 04-SQL/deploy.sh
+++ 04-SQL/deploy.sh
@@ -13,3 +13,30 @@ echo $TF_VAR_subnetPrivada
 echo $TF_VAR_keyPrivate
 echo $TF_VAR_amiId
 # --
+
+cd 04-SQL/terraform
+terraform init
+terraform apply -auto-approve
+
+echo "Aguardando criação de maquinas ..."
+sleep 10 # 10 segundos
+
+echo "[ec2-mysql-stag]" > ../ansible/hosts
+#echo "$(terraform output | grep mysql_instance_dev | awk '{print $2;exit}' | sed -e "s/\",//g")" >> ../ansible/hosts
+#echo "[ec2-mysql-stag]" >> ../ansible/hosts
+echo "$(terraform output | grep mysql_instance_stag | awk '{print $2;exit}' | sed -e "s/\",//g")" >> ../ansible/hosts
+#echo "[ec2-mysql-prod]" >> ../ansible/hosts
+#echo "$(terraform output | grep mysql_instance_prod | awk '{print $2;exit}' | sed -e "s/\",//g")" >> ../ansible/hosts
+
+sleep 10 # 10 segundos
+cd ../ansible
+
+echo "[client]" > dumpsql/.my.cnf
+echo "user=root" >> dumpsql/.my.cnf
+echo "password=$DB_PASSWORD" >> dumpsql/.my.cnf
+
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
+
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionarDataBase.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionarDataBase.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
