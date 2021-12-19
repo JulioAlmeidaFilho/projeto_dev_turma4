2cf5b5e9390842c6516f666d1b225196d16fc9be
jbosso <j_bosso@outlook.com>
2021-12-19 09:22:07 +0000

    message

--- 02-AMI/createAMI.sh
+++ 02-AMI/createAMI.sh
@@ -5,6 +5,6 @@ VERSAO=$(date +'%d_%m_%H_%M_%S')
 cd 02-AMI/terraform
 RESOURCE_ID=$(terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
 
-cd 02-AMI/terraformAMI
+cd ../terraformAMI
 terraform init
 TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID terraform apply -auto-approve
